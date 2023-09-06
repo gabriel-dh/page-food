7afb5918ba9f8b212ba18fe4f3702212d84fbc8f
gabriel-dh <gabrieldavidhuertas@gmail.com>
2023-09-05 21:55:48 -0500

    first commit

---
+++ scroll.js
@@ -0,0 +1,10 @@
+
+
+ScrollReveal().reveal('.header-txt',{ delay: 500 , distance: '80px', origin: 'left' });
+ScrollReveal().reveal('.about-img',{ delay: 1000 ,  scale: .2  });
+ScrollReveal().reveal('.about-txt',{ delay: 1200 , distance: '80px', origin: 'right'   });
+ScrollReveal().reveal('.image-1',{ delay: 600 , duration: 1000, opacity: 0 , distance: '70px', origin: 'top'});
+ScrollReveal().reveal('.image-2',{ delay: 800,  duration: 1500 , opacity: 0 , distance: '70px',origin: 'bottom' });
+ScrollReveal().reveal('.image-3',{ delay: 1000, duration: 1800 , opacity: 0 , distance: '70px', origin: 'top'  });
+ScrollReveal().reveal('.product-1',{ delay: 600 , duration: 1000 ,  scale: .2  });
+ScrollReveal().reveal('.tagline', { delay: 500 });
